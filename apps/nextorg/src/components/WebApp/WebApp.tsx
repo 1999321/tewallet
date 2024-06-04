986e18cc833d2ac34b2b28b72f3eabbe86302619
“1999321” <lizhao2250537362@gmail.com>
2024-06-04 11:58:05 +0800

    change nextorg

--- apps/nextorg/src/components/WebApp/WebApp.tsx
+++ apps/nextorg/src/components/WebApp/WebApp.tsx
@@ -6,7 +6,8 @@ export const WebApp = () => {
     const [initdata, setInitdata] = useState<String>("")
     useEffect(() => {
         setInitdata(window?.Telegram?.WebApp?.initdata)
-    }, [])
+        console.log("initdata:", initdata)
+    }, [initdata])
 
     return (
         <div>WebAppData{initdata}</div>
